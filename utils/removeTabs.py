0ca8cbf234359b81a162fb768bd0c09dc0208a62
juripapay <82206056+juripapay@users.noreply.github.com>
2022-04-20 07:37:38 +0100

    Update removeTabs.py

--- utils/removeTabs.py
+++ utils/removeTabs.py
@@ -1,1 +1,11 @@
+# Remove tabs from python file.
+
+inputFile = open('stemdl_light.py', "r")
+exportFile = open('stemdl_light_1.py', "w")
+for line in inputFile:
+   new_line = line.replace('\t', '    ')
+   exportFile.write(new_line)
+
+inputFile.close()
+exportFile.close()
 
